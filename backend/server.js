65aa4e1c0aff0478b7843d5a0eb8b42f9a47a18a
ricardomb94 <aymardmb@gmail.com>
2023-10-09 01:59:56 +0200

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue l'API est déployée avec succés ");
+});
+
+app.listen(port, () => console.log(`Le Server écoute sur le port ${port}`));
