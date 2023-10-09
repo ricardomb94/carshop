b58e9a313a8878900cc4aaa1ce1d513542eee75b
ricardomb94 <aymardmb@gmail.com>
2023-10-09 04:55:24 +0200

    Fetching dummy data from backend and use concurrently to run the backend and frontend at once

---
+++ backend/data/vehicules.js
@@ -0,0 +1,151 @@
+const vehicules = [
+  {
+    _id: "1",
+    name: "Toyota IQ",
+    images: [
+      {
+        original: "/images/yarris-1.jpeg",
+        thumbnail: "/thumbnails/yarris-t1.jpeg",
+      },
+      {
+        original: "/images/yarris-2.jpeg",
+        thumbnail: "/thumbnails/yarris-t2.jpeg",
+      },
+      {
+        original: "/images/yarris-3.jpeg",
+        thumbnail: "/thumbnails/yarris-t3.jpeg",
+      },
+      {
+        original: "/images/yarris-5.jpeg",
+        thumbnail: "/thumbnails/yarris-t5.jpeg",
+      },
+      {
+        original: "/images/yarris-6.jpeg",
+        thumbnail: "/thumbnails/yarris-t6.jpeg",
+      },
+    ],
+    description:
+      "La Toyota IQ est une voiture à hayon élégante et compacte qui respire la sophistication et la modernité. Elle présente un design extérieur élégant caractérisé par des lignes nettes, une calandre avant agressive et des phares à LED. Sa taille compacte en fait une voiture idéale pour la ville, permettant une maniabilité et un stationnement faciles.",
+
+    brand: "Toyota IQ",
+    year: 2009,
+    category: "Essence",
+    color: "noire",
+    countInStock: 10,
+    price: 7500,
+    rating: 2.5,
+    provenance: "Belgique",
+    registration: "12/09/2009",
+    vehicleInspection: "Requis",
+    originalOwner: "Oui",
+    odometerReading: "116.000 Km",
+    energy: "essence",
+    transmission: "Manuelle",
+    upholstery: "cuir/tissu",
+    doors: 3,
+    seats: 4,
+    // doors: 3,
+    // seats: 4,
+    numReviews: 25,
+  },
+  {
+    _id: "2",
+    name: "Volgswagen UP",
+    images: [
+      {
+        original: "/images/volgswagen-slider5.jpeg",
+        thumbnail: "/thumbnails/volgswagen-t5.jpeg",
+      },
+      {
+        original: "/images/volgswagen-s4.jpeg",
+        thumbnail: "/thumbnails/volgswagen-t4.jpeg",
+      },
+      {
+        original: "/images/volgswagen-s3.jpeg",
+        thumbnail: "/thumbnails/volgswagen-t3.jpeg",
+      },
+      {
+        original: "/images/volgswagen-s1.jpeg",
+        thumbnail: "/thumbnails/volgswagen-t1.jpeg",
+      },
+      {
+        original: "/images/volgswagen-s2.jpeg",
+        thumbnail: "/thumbnails/volgswagen-t2.jpeg",
+      },
+      {
+        original: "/images/volgswagen-s0.jpeg",
+        thumbnail: "/thumbnails/volgswagen-t0.jpeg",
+      },
+    ],
+    description:
+      "La Volswagen Up! est une petite voiture citadine compacte connue pour sa taille réduite, sa praticité et son efficacité. Elle arbore un design moderne, un intérieur étonnamment spacieux pour sa catégorie et diverses fonctionnalités et technologies modernes. Avec sa maniabilité réactive et ses options respectueuses de l'environnement, elle est bien adaptée à la conduite en ville et aux déplacements quotidiens.",
+
+    brand: "Volgswagen",
+    year: 2013,
+    category: "essence",
+    color: "blanche",
+    countInStock: 6,
+    price: 9000,
+    rating: 4.5,
+    provenance: "Allemagne",
+    registration: "12/09/2013 ",
+    vehicleInspection: "requis",
+    originalOwner: "Oui",
+    odometerReading: "66300 Km",
+    energy: "essence",
+    transmission: "manuelle",
+    upholstery: "cuir/tissu",
+    doors: 3,
+    seats: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Toyota Yaris",
+    images: [
+      // {
+      //   original:'/images/yarris-rouge-1.jpeg',
+      //   thumbnail:'/thumbnails/yarris-rouge-t1.jpeg',
+      // },
+      {
+        original: "/images/yarris-rouge-2.jpeg",
+        thumbnail: "/thumbnails/yarris-rouge-t2.jpeg",
+      },
+      {
+        original: "/images/yarris-rouge-3.jpeg",
+        thumbnail: "/thumbnails/yarris-rouge-t3.jpeg",
+      },
+      {
+        original: "/images/yarris-rouge-4.jpeg",
+        thumbnail: "/thumbnails/yarris-rouge-t4.jpeg",
+      },
+      // {
+      //   original:'/images/yarris-rouge-5.jpeg',
+      //   thumbnail:'/thumbnails/yarris-t5.jpeg',
+      // },
+    ],
+    description:
+      "La Toyota  Yaris est une voiture à hayon élégante et compacte qui respire la sophistication et la modernité. Elle présente un design extérieur élégant caractérisé par des lignes nettes, une calandre avant agressive et des phares à LED. Sa taille compacte en fait une voiture idéale pour la ville, permettant une maniabilité et un stationnement faciles.",
+
+    brand: "Toyota Yaris",
+    year: 2012,
+    category: "Essence",
+    color: "rouge",
+    countInStock: 0,
+    price: 8500,
+    rating: 4.5,
+    provenance: "Allemagne",
+    registration: "1/02/2012",
+    vehicleInspection: "Requis",
+    originalOwner: "Oui",
+    odometerReading: "130.000 Km",
+    energy: "essence",
+    transmission: "Manuelle",
+    upholstery: "cuir/tissu",
+    doors: 3,
+    seats: 5,
+    numReviews: 12,
+  },
+];
+
+export default vehicules;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from "express";
+import vehicules from "./data/vehicules.js";
+import { log } from "console";
 
-const port = 3000;
+const port = 5000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Bienvenue l'API est déployée avec succés ");
+});
+
+app.get("/api/vehicules", (req, res) => {
+  res.json(vehicules);
+});
+
+app.get("/api/vehicules/:id", (req, res) => {
+  const vehicule = vehicules.find((v) => v._id === req.params.id);
+  res.json(vehicule);
 });
 
 app.listen(port, () => console.log(`Le Server écoute sur le port ${port}`));
