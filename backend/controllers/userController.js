c4bd83abba6fd107d60cfa98a47cbe587e5c017f
ricardomb94 <aymardmb@gmail.com>
2023-11-13 21:17:18 +0100

    Another commit for logging out user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @routes POST /api/users
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Deconexion accepté" });
 });
 
 // @desc Get user profile
