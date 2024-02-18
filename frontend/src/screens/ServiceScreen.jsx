f77c643afa8845c545ac052a65f9d4d87e58b087
ricardomb94 <aymardmb@gmail.com>
2024-02-18 04:12:56 +0100

    git push origin services

--- frontend/src/screens/ServiceScreen.jsx
+++ frontend/src/screens/ServiceScreen.jsx
@@ -1,65 +1,18 @@
-// ServiceScreen.jsx
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { ScaleLoader } from "react-spinners";
-// import Message from "../components/Message";
-// import ServiceList from "../screens/admin/ServiceList";
-// import ServiceCreateScreen from "../screens/admin/ServiceCreateScreen";
-// import {
-//   useGetServicesQuery,
-//   useCreateServiceMutation,
-// } from "../slices/servicesApiSlice";
-
-// const ServiceScreen = () => {
-//   const { data: services, isLoading, error } = useGetServicesQuery();
-//   const [currentPage, setCurrentPage] = useState(1);
-
-//   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-//   return (
-//     <>
-//       <Link
-//         className='btn btn-light my-3 position-fixed bottom-0 end-0 m-4'
-//         to='/'
-//       >
-//         Voir le Catalogue
-//       </Link>
-
-//       <ServiceCreateScreen />
-
-//       {isLoading ? (
-//         <ScaleLoader
-//           visible={+true}
-//           height={40}
-//           width={5}
-//           color='#36d7b7'
-//           aria-label='scale-loading'
-//           wrapperstyle={{}}
-//           wrapperclass='scale-wrapper'
-//         />
-//       ) : error ? (
-//         <Message variant='danger'>
-//           {error?.data?.message || error.error}
-//         </Message>
-//       ) : (
-//         <ServiceList
-//           services={services}
-//           currentPage={currentPage}
-//           paginate={paginate}
-//         />
-//       )}
-//     </>
-//   );
-// };
-
-// export default ServiceScreen;
-
-import React from "react";
 import { Row, Col, Pagination, Card } from "react-bootstrap";
 import { Fade } from "react-awesome-reveal";
+import { useGetServicesQuery } from "../slices/servicesApiSlice";
+import Service from "../components/Service";
+import Message from "../components/Message";
+import { ScaleLoader } from "react-spinners";
+import { useState } from "react";
 
-const ServiceScreen = ({ services, currentPage, paginate }) => {
-  const ITEMS_PER_PAGE = 6;
+const ITEMS_PER_PAGE = 6;
+
+const ServiceScreen = () => {
+  const { data: services, isLoading, refresh, error } = useGetServicesQuery();
+  console.log("SERVICELIST", services);
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
   const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
@@ -67,47 +20,71 @@ const ServiceScreen = ({ services, currentPage, paginate }) => {
     ? services.slice(indexOfFirstItem, indexOfLastItem)
     : [];
 
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <>
       <h2 className='text-center'>
         <Fade cascade damping={0.1}>
-          Garage Services
+          Adamoautos votre garage de Services
         </Fade>
       </h2>
 
-      <Fade triggerOnce cascade>
-        <Row>
-          {currentItems.map((service) => (
-            <Col key={service._id} sm={12} md={6} lg={4} xl={4}>
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant='top' src={service.imageUrl} />
-                <Card.Body>
-                  <Card.Title>{service.title}</Card.Title>
-                  <Card.Text>{service.description}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Fade>
+      {isLoading ? (
+        <ScaleLoader
+          visible={+true}
+          height={40}
+          width={5}
+          color='#36d7b7'
+          aria-label='scale-loading'
+          wrapperstyle={{}}
+          wrapperclass='scale-wrapper'
+        />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          {/* <h2 className='text-center'>
+            <Fade cascade damping={0.1}>
+              Véhicules à vendre
+            </Fade>
+          </h2> */}
 
-      <Row className='justify-content-center'>
-        <Col>
-          <Pagination>
-            {Array.from({
-              length: Math.ceil(services.length / ITEMS_PER_PAGE),
-            }).map((_, index) => (
-              <Pagination.Item
-                key={index + 1}
-                active={index + 1 === currentPage}
-                onClick={() => paginate(index + 1)}
-              >
-                {index + 1}
-              </Pagination.Item>
-            ))}
-          </Pagination>
-        </Col>
-      </Row>
+          <Fade triggerOnce cascade>
+            <Row>
+              {currentItems.map((service) => {
+                const imagePath = `http://localhost:5000/${service.image}`;
+
+                return (
+                  <Col key={service._id} sm={12} md={6} lg={4} xl={4}>
+                    <Service service={service} imagePath={imagePath} />
+                  </Col>
+                );
+              })}
+            </Row>
+          </Fade>
+
+          <Row className='justify-content-center'>
+            <Col>
+              <Pagination>
+                {Array.from({
+                  length: Math.ceil(services.length / ITEMS_PER_PAGE),
+                }).map((_, index) => (
+                  <Pagination.Item
+                    key={index + 1}
+                    active={index + 1 === currentPage}
+                    onClick={() => paginate(index + 1)}
+                  >
+                    {index + 1}
+                  </Pagination.Item>
+                ))}
+              </Pagination>
+            </Col>
+          </Row>
+        </>
+      )}
     </>
   );
 };
