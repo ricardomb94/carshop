839784573c1fab925faffe4762ecf7fecb3ba5d8
ricardomb94 <aymardmb@gmail.com>
2024-03-11 07:46:07 +0100

    Cors set up

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(helmet());
 //Cors
 app.use(
   cors({
-    origin: "process.env.BASE_URL", // Allow requests from this origin
+    origin: process.env.BASE_URL, // Allow requests from this origin
     credentials: true, // Enable credentials (cookies, authorization headers, etc.)
   })
 );
@@ -55,7 +55,8 @@ app.use("/api/contact", contactRoutes)
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/services", serviceRoutes)
-const __dirname = path.resolve(); //Set __dirname to current directory
+const __dirname = path.resolve(); //Set __dirname to current directoryme
+console.log("CURRENT DIRECTORY __dirna :", __dirname)
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use("/thumbnails", express.static(path.join(__dirname, "/thumbnails")));
 app.use("/resized", express.static(path.join(__dirname, "/resized")));
