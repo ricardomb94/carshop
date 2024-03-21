b7df47d2c11ec76b5cb65ee1fffd22b8e05545ae
ricardomb94 <aymardmb@gmail.com>
2024-03-21 06:26:12 +0100

    Setup error diplay using a toastify notification

--- frontend/src/screens/ContactFormScreen.jsx
+++ frontend/src/screens/ContactFormScreen.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { Form, Row, Col, Button, Container } from "react-bootstrap";
-// import FormContainer from "../components/FormContainer";
 import { useSubmitContactFormMutation } from "../slices/contactApiSlice";
 import { ScaleLoader } from "react-spinners";
-import Message from "../components/Message";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const services = [
   "Service 1",
@@ -28,14 +28,29 @@ const ContactFormScreen = () => {
     selectedService: "",
   });
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     if (formData.selectedService) {
       // Submit the form
-      submitContactForm(formData);
+      try {
+        await submitContactForm(formData);
+        // Reset form fields after successful submission
+        setFormData({
+          name: "",
+          telephone: "",
+          email: "",
+          message: "",
+          selectedService: "",
+        });
+        toast.success("Form submitted successfully!");
+      } catch (error) {
+        console.error("Error submitting form:", error);
+        toast.error("Error submitting form: " + error.toString());
+      }
     } else {
       // Handle the case where no service is selected (e.g., display an error message)
       console.error("Please select a service.");
+      toast.error("Please select a service.");
     }
   };
 
@@ -48,14 +63,11 @@ const ContactFormScreen = () => {
     <Container fluid className='form-contact'>
       <h2 className='text-center pt-3'>Besoin d'aide ?</h2>
       <p className='text-center'>Laissez nous un message</p>
-      {/* <FormContainer className='contactForm'> */}
       {isLoading && <ScaleLoader />}
-      {error && <Message variant='danger'>{error.toString()}</Message>}
       <Form onSubmit={submitHandler}>
         <Row>
           <Col lg={6} md={12} sm={12}>
             <Form.Group controlId='name' className='my-3'>
-              {/* <Form.Label>Nom</Form.Label> */}
               <Form.Control
                 type='text'
                 name='name'
@@ -67,7 +79,6 @@ const ContactFormScreen = () => {
           </Col>
           <Col lg={6} md={12} sm={12}>
             <Form.Group controlId='telephone' className='my-3'>
-              {/* <Form.Label>Téléphone</Form.Label> */}
               <Form.Control
                 name='telephone'
                 type='text'
@@ -81,7 +92,6 @@ const ContactFormScreen = () => {
         <Row>
           <Col lg={6} md={12} sm={12}>
             <Form.Group controlId='service' className='my-3'>
-              {/* <Form.Label>Type de service</Form.Label> */}
               <Form.Control
                 name='service'
                 type='text'
@@ -100,7 +110,6 @@ const ContactFormScreen = () => {
           </Col>
           <Col lg={6} md={12} sm={12}>
             <Form.Group controlId='email' className='my-3'>
-              {/* <Form.Label>E-mail</Form.Label> */}
               <Form.Control
                 type='email'
                 name='email'
@@ -112,7 +121,6 @@ const ContactFormScreen = () => {
           </Col>
         </Row>
         <Form.Group controlId='message' className='my-3'>
-          {/* <Form.Label>Message</Form.Label> */}
           <Form.Control
             as='textarea'
             name='message'
@@ -127,7 +135,7 @@ const ContactFormScreen = () => {
           Envoyer
         </Button>
       </Form>
-      {/* </FormContainer> */}
+      <ToastContainer />
     </Container>
   );
 };
